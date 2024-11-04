c4979bcb60fb255509bea1d968b59d22bab9f766
Staiger, Christine <christine.staiger@wur.nl>
2024-11-04 19:06:22 +0100

    row wise check for column clusters

--- src/validate.py
+++ src/validate.py
@@ -3,88 +3,100 @@
 import argparse
 import tomllib
 import sys
-import pandas as pd
 import pprint
-
 from pathlib import Path
+import pandas as pd
 
-from validation_utils import (check_column_types, check_column_exists,
-                              check_column_clusters, identifier_checks,
-                              print_output)
+from validation_utils import (
+    check_column_types,
+    check_column_exists,
+    check_column_clusters,
+    identifier_checks,
+    print_output,
+)
 
 # Location of this file
 PATH = Path.absolute(Path(__file__))
 
 # Files to validate
-CSV_FILE_NAMES = ['environment_events.csv', 'environment.csv', 'host_events.csv', 'hosts.csv']
+CSV_FILE_NAMES = [
+    "environment_events.csv",
+    "environment.csv",
+    "host_events.csv",
+    "hosts.csv",
+]
 
 # Max number of results printed to screen
 MAX_NUM_RES = 25
 COUNT = 0
 
+
 def main() -> None:
     """CLI."""
     global COUNT
     parser = argparse.ArgumentParser(
-                prog="CSV file validation",
-                description="Validates the formatting and typing of csv files."
-             )
-    
-    parser.add_argument(
-            "-d",
-            "--data",
-            help="The path to the data folder containing the csv files.",
-            type=Path,
-            required=True
-            )
+        prog="CSV file validation",
+        description="Validates the formatting and typing of csv files.",
+    )
 
     parser.add_argument(
-            "-c",
-            "--configfile",
-            help="The path to the validation.toml.",
-            type=Path,
-            default=PATH.parent.parent / "data" / "metadata_test" / "_validation_schema_v2.toml"
-            )
+        "-d",
+        "--data",
+        help="The path to the data folder containing the csv files.",
+        type=Path,
+        required=True,
+    )
 
     parser.add_argument(
-            "-s",
-            "--sep",
-            help="Separator between columns in csv files. \n Tab separator: tab. Default: ;",
-            type=str,
-            default=";"
-            )
+        "-c",
+        "--configfile",
+        help="The path to the validation.toml.",
+        type=Path,
+        default=PATH.parent.parent
+        / "data"
+        / "metadata_test"
+        / "_validation_schema_v2.toml",
+    )
 
     parser.add_argument(
-            "-v",
-            "--verbose",
-            help="Get more output in the single steps of the pipeline to debug.",
-            action='store_true'
-            )
+        "-s",
+        "--sep",
+        help="Separator between columns in csv files. \n Tab separator: tab. Default: ;",
+        type=str,
+        default=";",
+    )
 
-    args, _ = parser.parse_known_args() 
-   
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Get more output in the single steps of the pipeline to debug.",
+        action="store_true",
+    )
+
+    args, _ = parser.parse_known_args()
+
     # For tab separated files
     if args.sep == "tab":
         args.sep = "\t"
 
     # read in toml file
     validation = read_toml(args.configfile)
-    if args.verbose == True:
-        print(f"DEBUG: validation.toml.")
+    if args.verbose is True:
+        print("DEBUG: validation.toml.")
         pprint.pprint(validation)
     # read in data
     data = read_csv_files(args.data, args.sep)
-    if args.verbose == True:
-        for df_name in data:
-            print(f"DEBUG:shape of {df_name}: {data[df_name].shape}")
+    if args.verbose is True:
+        for df_name, df in data:
+            print(f"DEBUG:shape of {df_name}: {df.shape}")
     print("Read in csv files: done.")
 
     # check if all columns exist
     result = check_column_exists(data, validation)
     for fname, col in result:
         print_output(f"Missing column {col} in {fname}.", "fail")
-        COUNT+=1
-        if COUNT == MAX_NUM_RES-1:
+        COUNT += 1
+        if COUNT == MAX_NUM_RES - 1:
             print(f"First {MAX_NUM_RES} results out of {len(result)}")
             break
     COUNT = 0
@@ -95,13 +107,12 @@ def main() -> None:
     for fname, col, found_type, exp_type in result:
         msg = f"TYPE in {fname} column {col}; expected {exp_type}, found {found_type}."
         print_output(msg, "fail")
-        COUNT+=1
-        if COUNT == MAX_NUM_RES-1:
+        COUNT += 1
+        if COUNT == MAX_NUM_RES - 1:
             print(f"First {MAX_NUM_RES} results out of {len(result)}")
             break
     COUNT = 0
     print("Check column types completed.")
-
 
     # checks on identifier columns
     ids = ["host_id", "environment_id"]
@@ -109,25 +120,50 @@ def main() -> None:
         print("Identifier columns checked successfully.")
     else:
         print("Errors found in identifier columns. Please correct them.")
-        #sys.exit()
+        # sys.exit()
 
     # check dependencies between columns
     for data_name, df in data.items():
         if set(validation["column_dependencies"]["event"]).issubset(df.columns):
-            measure = set(validation["column_dependencies"]["measurement"]).issubset(df.columns)
-            inoc = set(validation["column_dependencies"]["inoculation"]).issubset(df.columns)
-            treat = set(validation["column_dependencies"]["treatment"]).issubset(df.columns)
+            measure = set(validation["column_dependencies"]["measurement"]).issubset(
+                df.columns
+            )
+            inoc = set(validation["column_dependencies"]["inoculation"]).issubset(
+                df.columns
+            )
+            treat = set(validation["column_dependencies"]["treatment"]).issubset(
+                df.columns
+            )
             if not (measure or inoc or treat):
                 print(f"{data_name}: Need also information on either of measurement,")
                 print("inoculation or treatment.")
     print("Check if column clusters are present completed.")
 
+    # check rows in each cluster of columns and return rows where single values are missing
+    for data_name, df in data.items():
+        results = check_column_clusters(validation["column_dependencies"], df)
+        if len(results) > 0:
+            print(f"{data_name}: found missing values")
+            for index, cols in results:
+                print(f"Row {index} in columns {cols}.")
+                COUNT += 1
+                if COUNT == MAX_NUM_RES - 1:
+                    print(f"First {MAX_NUM_RES} results out of {len(result)}")
+                    break
+            COUNT = 0
+
+
 def read_csv_files(path: Path, sep: str) -> dict:
+    """Read in csv files."""
     if not path.is_dir():
         raise FileNotFoundError(f"{path} does not exist.")
-    files = [f.name for f in path.glob('**/*.csv') if f.is_file() and f.name in CSV_FILE_NAMES]
+    files = [
+        f.name
+        for f in path.glob("**/*.csv")
+        if f.is_file() and f.name in CSV_FILE_NAMES
+    ]
     if not set(files) == set(CSV_FILE_NAMES):
-        msg = f"{FAIL}Missing files. Expected {CSV_FILE_NAMES}\nFound {files}{ENDC}"
+        msg = f"Missing files. Expected {CSV_FILE_NAMES}\nFound {files}"
         raise FileNotFoundError(msg)
 
     data = {}
@@ -135,12 +171,14 @@ def read_csv_files(path: Path, sep: str) -> dict:
         try:
             data[f] = pd.read_csv(path.joinpath(f), sep=sep)
         except pd.errors.EmptyDataError as err:
-            raise ValueError(f"{path.joinpath(f)} is empty.")
+            raise ValueError(f"{path.joinpath(f)} is empty.") from err
         except Exception as err:
-            raise ValueError(f"Reading {path.joinpath(f)} failed. {repr(err)}")
+            raise ValueError(f"Reading {path.joinpath(f)} failed. {repr(err)}") from err
     return data
 
+
 def read_toml(path: Path) -> dict:
+    """Read in toml file."""
     print_output(f"reading validation file: {path}.", "info")
     try:
         with open(path, "rb") as f:
@@ -152,6 +190,7 @@ def read_toml(path: Path) -> dict:
         _, ex_value, _ = sys.exc_info()
         raise ValueError(f"Wrong fromat of validation file: {ex_value}.") from err
     return validation
+
 
 if __name__ == "__main__":
     main()
--- src/validation_utils.py
+++ src/validation_utils.py
@@ -1,9 +1,11 @@
+"""Functionality for the validatuion pipeline."""
 import pandas as pd
 
 # Colors
-INFO = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
+INFO = "\033[93m"
+FAIL = "\033[91m"
+ENDC = "\033[0m"
+
 
 def print_output(text: str, type_out: str):
     """Print text as info or error."""
@@ -26,6 +28,7 @@ def type_is_equal(col_type: str, val_type: str) -> bool:
         return True
     return False
 
+
 def check_column_types(data: dict, validation: dict) -> list:
     """Iterate over all csv files and check whether columns are well formatted.
 
@@ -44,7 +47,7 @@ def check_column_types(data: dict, validation: dict) -> list:
 
     """
     output = []
-    
+
     for data_name, df in data.items():
         if data_name == "host_events.csv":
             section = "events"
@@ -59,18 +62,28 @@ def check_column_types(data: dict, validation: dict) -> list:
                     # compare the actual type to each of the types in the list
                     res = [type_is_equal(str(df[var_name].dtype), t) for t in type_list]
                     if not any(res):
-                        output.append((data_name, var_name,
-                                       str(df[var_name].dtype),
-                                       validation[section][var_name]['type']
-                                       ))
+                        output.append(
+                            (
+                                data_name,
+                                var_name,
+                                str(df[var_name].dtype),
+                                validation[section][var_name]["type"],
+                            )
+                        )
                 else:
-                    if not type_is_equal(str(df[var_name].dtype),
-                                     validation[section][var_name]["type"]):
-                        output.append((data_name, var_name,
-                                       str(data[data_name][var_name].dtype),
-                                       validation[section][var_name]['type']
-                                       ))
+                    if not type_is_equal(
+                        str(df[var_name].dtype), validation[section][var_name]["type"]
+                    ):
+                        output.append(
+                            (
+                                data_name,
+                                var_name,
+                                str(data[data_name][var_name].dtype),
+                                validation[section][var_name]["type"],
+                            )
+                        )
     return output
+
 
 def check_column_exists(data: dict, validation: dict) -> list:
     """Iterate over all csv files and check if all columns are present.
@@ -94,12 +107,14 @@ def check_column_exists(data: dict, validation: dict) -> list:
         else:
             section = data_name.split(".")[0]
         for var_name in validation[section]:
-            if not var_name in df.columns:
+            if var_name not in df.columns:
                 missing.append((data_name, var_name))
     return missing
 
-def check_column_clusters(cols: dict, data_frame: pd.DataFrame):
-    """Check if rows of cluster columns are defined, return row and col names if not.
+
+def check_column_clusters(cols: dict, data_frame: pd.DataFrame) -> list:
+    """Check if row contains values in all columns of the cluster.
+    Return row and col names if not.
 
     cols: dict
         Mapping from cluster name to column names.
@@ -115,13 +130,16 @@ def check_column_clusters(cols: dict, data_frame: pd.DataFrame):
                     result.append((index, items))
     return result
 
+
 def unique_values(df: pd.DataFrame, col_name: str):
     """Check if all values in the column are unique and not None."""
     return df[col_name].nunique() == len(df[col_name])
 
+
 def find_none_values(df: pd.DataFrame, col_name: str):
     """Check if all values in the column contains None."""
-    return(df[df[col_name].isnull()].index.tolist())
+    return df[df[col_name].isnull()].index.tolist()
+
 
 def identifier_checks(data: dict, id_col_names: list) -> bool:
     """Check identifier columns."""
@@ -136,24 +154,28 @@ def identifier_checks(data: dict, id_col_names: list) -> bool:
                 print("Completed check for empty cells in ID columns.")
                 return False
 
-    # check if ids in host.csv and environment.csv are unique 
+    # check if ids in host.csv and environment.csv are unique
     if not unique_values(data["hosts.csv"], "host_id"):
         print_output("hosts.csv: Column host_id contains duplicates.", "fail")
         return False
     if not unique_values(data["environment.csv"], "environment_id"):
-        print_output("environment.csv: Column environment_id contains duplicates.", "fail")
+        print_output(
+            "environment.csv: Column environment_id contains duplicates.", "fail"
+        )
         return False
-    
-    #TODO: generalise the tuple (file, id_col)
+
+    # TODO: generalise the tuple (file, id_col)
     ids = {}
     ids["host_id"] = data["hosts.csv"]["host_id"].unique()
     ids["environment_id"] = data["environment.csv"]["environment_id"].unique()
-    
+
     for col_name in id_col_names:
         for data_name in [d for d in data if col_name in data[d]]:
             if not set(data[data_name][col_name]).issubset(ids[col_name]):
-                msg = f"File {data_name} contains undefined ids in column {col_name}:\n"+ \
-                      f"{set(data[data_name][col_name]).difference(ids[col_name])}"
+                msg = (
+                    f"File {data_name} contains undefined ids in column {col_name}:\n"
+                    + f"{set(data[data_name][col_name]).difference(ids[col_name])}"
+                )
                 print_output(msg, "fail")
                 return False
 
